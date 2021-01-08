f8965b7cba33809437436980e94c5b2ad7484cce
marcoluisnc <60854600+marcoluisnc@users.noreply.github.com>
2021-01-07 20:00:34 -0600

    new

---
+++ crecimiento/js/menuHam.js
@@ -0,0 +1,6 @@
+function openNav() {
+  document.querySelector(".menu-responsive").style.width = "100%";
+}
+function closeNav() {
+  document.querySelector(".menu-responsive").style.width = "0%";
+}
